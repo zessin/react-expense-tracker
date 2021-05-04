509a670ab6df57622637059ddd408d74ac33189d
zessin <gabrielzessin@hotmail.com>
2021-05-03 22:35:37 -0300

    Adding some components and modifying index page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from "./components/expenses/Expenses"
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      title: "Nintendo Switch",
+      amount: 300,
+      date: new Date(2021, 0, 5),
+    },
+    {
+      title: "TV",
+      amount: 799.99,
+      date: new Date(2021, 1, 10),
+    },
+    {
+      title: "Headset",
+      amount: 90,
+      date: new Date(2021, 2, 15),
+    },
+    {
+      title: "Keyboard",
+      amount: 79.99,
+      date: new Date(2021, 3, 20),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 align="center">Expense Tracker App</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
