1786cd0ee7c3814c100649a2266459b17b0b0272
zessin <gabrielzessin@hotmail.com>
2021-05-17 12:02:16 -0300

    Adding button for rendering new expenses form dynamically

--- src/components/expenses/NewExpense.js
+++ src/components/expenses/NewExpense.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   const saveExpenseDataHandler = (newExpenseData) => {
     const expenseData = {
       ...newExpenseData,
       id: Math.random().toString(),
     };
+
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button type="submit" onClick={startEditingHandler}>
+          New expense
+        </button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onCancel={stopEditingHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      )}
     </div>
   );
 };
